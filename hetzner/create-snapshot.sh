e1390e0289e20d571a8782f55a74659d3f35d4c3
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-05-08 20:49:42 +0200

    chore: refactor create-snapshot.sh to use long options for parameters
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- hetzner/create-snapshot.sh
+++ hetzner/create-snapshot.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
-media_path="$1"
-token="$2"
+# Set default values
+token=""
+media_path=""
 
-if [ -z "$token" ] || [ -z "$media_path" ]; then
-  echo "Usage: $0 <media_path> <token>"
+usage() {
+  echo "Usage: $0 --media-path <media_path> --token <token>"
   echo ""
   echo "Where:"
-  echo "  <media_path> is the path to the media file"
-  echo "  <token> is the Hetzner Cloud API token for a Hetzner Cloud project"
+  echo "  --token <token> is the Hetzner Cloud API token for a Hetzner Cloud project"
+  echo "  --media-path <media_path> is the path to the media file"
   exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  --token)
+    token="$2"
+    shift
+    ;;
+  --media-path)
+    media_path="$2"
+    shift
+    ;;
+  *) usage ;;
+  esac
+  shift
+done
+
+if [ -z "$media_path" ] || [ -z "$token" ]; then
+  usage
 fi
 
-export HCLOUD_TOKEN=$2
+export HCLOUD_TOKEN=$token
 packer init "$media_path"
 packer build "$media_path"
