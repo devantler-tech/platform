ff6fd5b277f020ecacd2b2c5ffa5120705289172
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2025-07-15 16:38:05 +0200

    feat: enhance create-server script usage instructions and add hcloud configuration file
    Signed-off-by: Nikolai Emil Damm <nikolaiemildamm@icloud.com>

--- hetzner/create-server.sh
+++ hetzner/create-server.sh
@@ -5,16 +5,20 @@ server_name=""
 image_id=""
 server_type="cax21"
 location="fsn1"
+placement_group="default"
+ssh_key_name="ssh-key"
 
 usage() {
-  echo "Usage: $0 --token <token> --server-name <server_name> --server-type <server_type> --location <location> --placement-group <placement_group> --image-id <image_id> --ssh-key-name <ssh_key_name>"
+  echo "Usage: $0 --token <token> --server-name <server_name> --image-id <image_id> [--server-type <server_type>] [--location <location>] [--placement-group <placement_group>] [--ssh-key-name <ssh_key_name>]"
   echo ""
   echo "Where:"
   echo "  --token <token> is the Hetzner Cloud API token for a Hetzner Cloud project"
   echo "  --server-name <server_name> is the name of the server to create"
-  echo "  --server-type <server_type> is the type of server to create e.g. cx11"
-  echo "  --location <location> is the location to create the server in e.g. fsn1"
   echo "  --image-id <image_id> is the ID of the snapshot image to use for the server"
+  echo "  --server-type <server_type> is the type of server to create. Default: cax21"
+  echo "  --location <location> is the location to create the server in. Default: fsn1"
+  echo "  --placement-group <placement_group> is the name of the placement group to use for the server. Default: default"
+  echo "  --ssh-key-name <ssh_key_name> is the name of the SSH key to use for the server. Default: ssh-key"
   exit 1
 }
 
@@ -57,7 +61,7 @@ if [ -z "$token" ] || [ -z "$server_name" ] || [ -z "$server_type" ] || [ -z "$l
   usage
 fi
 
-export HCLOUD_TOKEN=$1
+export HCLOUD_TOKEN="$token"
 
 hcloud context create default
 
@@ -87,4 +91,4 @@ hcloud server create --name "$server_name" \
   --image "$image_id" \
   --network default \
   --firewall talos-firewall \
-  --ssh-key ssh-key
+  --ssh-key "$ssh_key_name" \
